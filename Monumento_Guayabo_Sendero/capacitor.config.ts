9538f1a72c1f7b4aec9878750aff621cadba5107
Adrián Mora <AdrianMoraMonge@gmail.com>
2022-07-19 14:56:40 -0600

    project creation

---
+++ Monumento_Guayabo_Sendero/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Monumento_Guayabo_Sendero',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
