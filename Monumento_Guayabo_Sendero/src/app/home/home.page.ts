a7bf9c4a5c76b949e687f3cb1991d22e1ded8a7a
Adrián Mora <AdrianMoraMonge@gmail.com>
2022-09-05 22:04:17 -0600

    welcome screen design

--- Monumento_Guayabo_Sendero/src/app/home/home.page.ts
+++ Monumento_Guayabo_Sendero/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HostListener } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nameForm: FormGroup;
+  scrWidth:any;
+  smallSize:boolean = false;
 
-  constructor() {}
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.scrWidth = window.innerWidth;
+    if(this.scrWidth <= 405)
+      this.smallSize = true;
+    else
+      this.smallSize = false;
+  }
 
-  public dio(a){
-    console.log(a)
+  constructor(private fb: FormBuilder) {
+    this.getScreenSize();
+    this.nameForm = this.fb.group({
+      name: [null, [Validators.required, Validators.minLength(3)]]
+    });
   }
 }
