c59596ab7ec9f410bbb752aa264b93c8fb8ae046
Adrián Mora <AdrianMoraMonge@gmail.com>
2022-09-13 16:25:20 -0600

    arrangement of maps

--- Monumento_Guayabo_Sendero/src/app/pages/map/map.page.ts
+++ Monumento_Guayabo_Sendero/src/app/pages/map/map.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  maps: string[] = ["assets/first_map.svg", "assets/second_map.svg", "assets/third_map.svg", "assets/fourth_map.svg", "assets/fifth_map.svg", "assets/sixth_map.svg", "assets/seventh_map.svg", "assets/eighth_map.svg"];
+  current_map: string = this.maps[0];
+
   constructor() { }
 
   ngOnInit() {
