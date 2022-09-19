a179facb9a7ce1119d34ca1e4ec71c941d9728a9
Adrián Mora <AdrianMoraMonge@gmail.com>
2022-09-19 09:00:24 -0600

    advance confirmation

--- Monumento_Guayabo_Sendero/src/app/pages/map/map.page.ts
+++ Monumento_Guayabo_Sendero/src/app/pages/map/map.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivitiesService } from 'src/app/services/activities/activities.service';
 import { AlertController } from '@ionic/angular';
 import { CookieService } from 'ngx-cookie-service'; 
+import { Subscription } from 'rxjs-compat/Subscription';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
 import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
@@ -14,9 +17,14 @@ export class MapPage implements OnInit {
   maps: string[] = ["assets/first_map.svg", "assets/second_map.svg", "assets/third_map.svg", "assets/fourth_map.svg", "assets/fifth_map.svg", "assets/sixth_map.svg", "assets/seventh_map.svg", "assets/eighth_map.svg"];
   current_map: string = this.maps[0];
   next_activity: number;
+  private _routerSub = Subscription.EMPTY;
 
   constructor(private activitiesService: ActivitiesService, private alertController: AlertController, private cookieService: CookieService, private router: Router) { 
-    this.confirmTour();
+    this._routerSub = this.router.events
+      .filter(event => event instanceof NavigationEnd && event.url == '/map')
+      .subscribe((value) => {
+        this.confirmTour();
+    });
   }
 
   ngOnInit() {
@@ -57,6 +65,7 @@ export class MapPage implements OnInit {
           let list = res as [{Result}];
           if(list != null && list.length > 0){
             let activitiesSolved = list[0].Result;
+            console.log(activitiesSolved);
             if(activitiesSolved >= 0){
               this.next_activity = activitiesSolved;
               this.current_map = this.maps[activitiesSolved];
