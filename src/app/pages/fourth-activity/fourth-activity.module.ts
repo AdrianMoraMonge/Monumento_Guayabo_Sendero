64b39c925095edc6af96749d6d8063c8a18a2b94
Adrián Mora <AdrianMoraMonge@gmail.com>
2022-10-13 15:54:38 -0600

    finished activity

--- src/app/pages/fourth-activity/fourth-activity.module.ts
+++ src/app/pages/fourth-activity/fourth-activity.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule, Validators } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,6 +11,7 @@ import { FourthActivityPage } from './fourth-activity.page';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     FourthActivityPageRoutingModule
