c1c24a8651963281d1a4b5e8ad70e94c026a87e7
Adrián Mora <AdrianMoraMonge@gmail.com>
2022-10-15 10:41:03 -0600

    page creation

---
+++ src/app/pages/sixth-activity/sixth-activity.page.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivitiesService } from 'src/app/services/activities/activities.service';
+import { AlertController } from '@ionic/angular';
+import { CookieService } from 'ngx-cookie-service';
+import { ModalController } from '@ionic/angular';
+//import { SixthActivityModalComponent } from './sixth-activity-modal/sixth-activity-modal.component';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
+import { Subscription } from 'rxjs-compat/Subscription';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter'; 
+@Component({
+  selector: 'app-sixth-activity',
+  templateUrl: './sixth-activity.page.html',
+  styleUrls: ['./sixth-activity.page.scss'],
+})
+export class SixthActivityPage implements OnInit {
+  private _routerSub = Subscription.EMPTY;
+
+  constructor(private activitiesService: ActivitiesService, private alertController: AlertController, private cookieService: CookieService, private modalCtrl: ModalController, private router: Router) {
+    this._routerSub = this.router.events
+      .filter(event => event instanceof NavigationEnd && event.url == '/fifth-activity')
+      .subscribe((value) => {
+        //this.confirmTour();
+    });
+   }
+
+  ngOnInit() {
+  }
+
+  public async presentAlert(title: string, msg: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'alert_style',
+      header: title,
+      message: msg,
+      buttons: ['Entendido']
+  });
+    await alert.present();
+  }
+
+  back(){
+    this.router.navigateByUrl("map");
+  }
+
+  public async confirmAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'alert_style',
+      header: "Confirmar",
+      message: "¿Estás seguro de su respuesta?",
+      buttons: [
+        {
+          text: 'No, cancelar',
+          role: 'cancel',
+          cssClass: 'cancel-button',
+        },
+        {
+          text: 'Sí, confirmar',
+          handler: () => {
+            this.completeActivity();
+          }  
+        }
+      ]
+  });
+
+  await alert.present();
+}
+
+  async openModal(_points: number) {
+    /*const modal = await this.modalCtrl.create({
+      cssClass: 'remember_modal',
+      component: SixthActivityModalComponent,
+      componentProps: {
+        points: _points
+      }
+    });
+    modal.present();
+    const {data, role} = await modal.onWillDismiss();
+    if(role == "continue"){
+      this.router.navigateByUrl("map");
+      return;
+    }
+    this.openModal(_points);*/
+  }
+
+  completeActivity(){
+    if(this.cookieService.check('idUser')) {
+      this.activitiesService.checkActivity({_idUser: this.cookieService.get('idUser'), answer: "this.user_response", id_excercise: 5})
+        .subscribe(res => {
+          let list = res as [{Result}];
+          if(list != null && list.length > 0){
+            let points = list[0].Result;
+            if(points >= 0){
+              this.openModal(points);
+              return;
+            }
+          }
+          this.presentAlert('Error', 'Ocurrió un error, intente de nuevo.');
+      });
+    }
+    else
+      this.router.navigateByUrl('home');
+  }
+
+  confirmTour(){
+    if(this.cookieService.check('idUser')) {
+      this.activitiesService.numberActivitiesSolved({_idUser: this.cookieService.get('idUser')})
+        .subscribe(res => {
+          let list = res as [{Result}];
+          if(list != null && list.length > 0){
+            let activitiesSolved = list[0].Result;
+            if(activitiesSolved != 4){
+              this.router.navigateByUrl('map');
+            }
+            return;
+          }
+          this.presentAlert('Error', 'Ocurrió un error, intente de nuevo.');
+      });
+      return;
+    }
+    this.router.navigateByUrl('home');
+  }
+}
